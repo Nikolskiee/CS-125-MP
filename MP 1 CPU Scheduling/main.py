6738ea661049ce0fdaf09b650ac08fd6e24a1893
Nichol <nicholjohnfamadico@gmail.com>
2021-06-01 00:32:21 +0800

    Update main.py

--- MP 1 CPU Scheduling/main.py
+++ MP 1 CPU Scheduling/main.py
@@ -9,11 +9,10 @@ class Process:
         self.response = 0
         self.waiting = 0
         self.turnaround = 0
-        self.running = False
-        self.waiting = False
+        self.remaining = self.burst
     
 
-def fcfs():
+def fcfs(): #First Come First Serve
     count = int(input("Enter the number of processes: "))
     i = 1
     time = 0
@@ -30,7 +29,186 @@ def fcfs():
         processes.append(process)
         i = i + 1
 
-    t = 0
+    t = 0 #time
+    ready = [] #ready queue
+    active = None
+    gantt = [] #Gantt Chart
+
+    while (t <= time):
+        for x in processes:
+            if (x.arrival == t):
+                ready.append(x)
+
+        if(active != None):
+            if (t == active.completion):
+                active.turnaround = active.completion - active.arrival
+                active = None
+            
+        if (len(ready) != 0 and active == None):
+            active = ready[0]
+            for x in ready:
+                if (x.arrival < active.arrival):
+                    active = x
+                elif((x.arrival == active.arrival) and (x.priority < active.priority)):
+                    active = x
+                else:
+                    continue
+            
+            ready.remove(active)
+            gantt.append(active)
+            active.response = (t - active.arrival)
+            active.waiting = active.response
+            active.completion = t + active.burst
+        
+        t += 1
+
+    print("Gantt")
+    for x in gantt:
+        print(x.name, x.completion)
+
+    print("Pro \t Bur \t Arr \t Com \t Res \t Wai \t Tur")
+    for x in processes:
+        print(x.name, "\t", x.burst, "\t", x.arrival, "\t", x.completion, "\t", x.response, "\t", x.waiting, "\t", x.turnaround)
+
+def sjf(): #Shortest Job First
+    count = int(input("Enter the number of processes: "))
+    i = 1
+    time = 0
+    processes = []
+    while(i <= count):
+        name = "P" + str(i)
+        print(name)
+        priority = i
+        arrival = int(input("Arrival time: "))
+        burst = int(input("Burst time: "))
+        time += burst
+        
+        process = Process(name, priority, arrival, burst)
+        processes.append(process)
+        i = i + 1
+
+    t = 0 #time
+    ready = [] #ready queue
+    active = None
+    gantt = [] #Gantt Chart
+
+    while (t <= time):
+        for x in processes:
+            if (x.arrival == t):
+                ready.append(x)
+
+        if(active != None):
+            if (t == active.completion):
+                active.turnaround = active.completion - active.arrival
+                active = None
+            
+        if (len(ready) != 0 and active == None):
+            active = ready[0]
+            for x in ready:
+                if (x.burst < active.burst):
+                    active = x
+                elif((x.burst == active.burst) and (x.priority < active.priority)):
+                    active = x
+                else:
+                    continue
+            
+            ready.remove(active)
+            gantt.append(active)
+            active.response = (t - active.arrival)
+            active.waiting = active.response
+            active.completion = t + active.burst
+        
+        t += 1
+
+    print("Gantt")
+    for x in gantt:
+        print(x.name, x.completion)
+
+    print("Pro \t Bur \t Arr \t Com \t Res \t Wai \t Tur")
+    for x in processes:
+        print(x.name, "\t", x.burst, "\t", x.arrival, "\t", x.completion, "\t", x.response, "\t", x.waiting, "\t", x.turnaround)
+
+def strf(): #Shortest Remaining Time First
+    count = int(input("Enter the number of processes: "))
+    i = 1
+    time = 0
+    processes = []
+    while(i <= count):
+        name = "P" + str(i)
+        print(name)
+        priority = i
+        arrival = int(input("Arrival time: "))
+        burst = int(input("Burst time: "))
+        time += burst
+        
+        process = Process(name, priority, arrival, burst)
+        processes.append(process)
+        i = i + 1
+
+    t = 0 #time
+    ready = [] #ready queue
+    active = None
+    gantt = [] #Gantt Chart
+
+    while (t <= time):
+
+        if(len(ready) != 0):
+            for x in ready:
+                x.waiting += 1
+        
+        for x in processes:
+            if (x.arrival == t):
+                ready.append(x)
+
+        if(active != None):
+            active.remaining -= 1
+            if (active.remaining == 0):
+                active.completion = t
+                active.turnaround = active.completion - active.arrival
+                active = None
+                gantt.append(str(t))
+            else:
+                buffer = active
+                for x in ready:
+                    if (x.remaining < active.remaining):
+                        active = x
+                    elif((x.remaining == active.remaining) and (x.priority < active.priority)):
+                        active = x
+                    else:
+                        continue
+                
+                if(buffer != active):
+                    ready.append(buffer)
+                    gantt.append(str(t))
+                    gantt.append(active.name)
+
+                    if(active.response == 0): active.response = (t - active.arrival)
+
+            
+        if (len(ready) != 0 and active == None):
+            active = ready[0]
+            for x in ready:
+                if (x.remaining < active.remaining):
+                    active = x
+                elif((x.remaining == active.remaining) and (x.priority < active.priority)):
+                    active = x
+                else:
+                    continue
+            
+            ready.remove(active)
+            gantt.append(active.name)
+            active.response = (t - active.arrival)
+
+        t += 1
+
+    print("Gantt")
+    for x in gantt:
+        print(x)
+
+    print("Pro \t Bur \t Arr \t Com \t Res \t Wai \t Tur")
+    for x in processes:
+        print(x.name, "\t", x.burst, "\t", x.arrival, "\t", x.completion, "\t", x.response, "\t", x.waiting, "\t", x.turnaround)
+
 
 def main():
     print("CPU Process Scheduling")
@@ -39,7 +217,7 @@ def main():
     print("Pick a CPU Scheduling Algorithm")
     print("1. First-Come First-Served")
     print("2. Shortest-Job-First")
-    print("3. Shortest-Time-First")
+    print("3. Shortest-Remaining-Time-First")
     print("4. Priority Schediling (Non-Preemptive")
     print("5. Priority Scheduling (Preemptive)")
     print("6. Round Robin")
@@ -53,6 +231,10 @@ def main():
     
     if(choice == 1):
         fcfs()
+    if(choice == 2):
+        sjf()
+    if(choice == 3):
+        strf()
 
 
 main()
